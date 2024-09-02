5a786abe052130179a0abf2075df190b6a22d2b1
Amandine Latour <natsukichan13@hotmail.fr>
2024-09-02 14:55:48 +0200

    release day11

--- 00/calendar-script.js
+++ 00/calendar-script.js
@@ -1,10 +1,8 @@
 const totalDays = 14; // Total days (including the last special day)
-let currentDay = 10; // Set current day
+let currentDay = 11; // Set current day
 let totalPoints = 66;
 
-// Day1: 31 (6/10) / Day2: 17 (6/10) / Dat3: 124 (10/10) / Day4: 32 (5/10) / Day5: 12 (5/10)
-// Day 6: 8 (9/10) / Day 7: 42 (7) / Day 8: 6 (6)
-// let questScore = {day1: 6, day2: 6, day3: 10, day4: 5, day5: 5, day6: 9, day7: 7, day8: 6}
+// let questScore = {day1: 6, day2: 6, day3: 10, day4: 5, day5: 5, day6: 9, day7: 7, day8: 6, day9: 7; day10: 5}
 
 let winTheContest;
 
